00008db808e17e15e6ce5b43ee74a2cb0bb8936b
chrisknsmn <116185710+chrisknsmn@users.noreply.github.com>
2025-10-13 12:38:55 -0400

    homepage button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let variant: 'default' | 'outline'  = 'default';
+    export let type: 'button' | 'submit'  = 'button';
+    export let disabled = false;
+</script>
+
+<button
+{type}
+{disabled}
+class="
+    px-4 py-2 rounded-md font-medium bg-black text-white cursor-pointer
+    {variant === 'default' ? 'border-0 bg-black text-white' : ''}
+    {variant === 'outline' ? 'border-2 bg-black text-white' : ''}
+    "
+  on:click
+>
+    <slot />
+</button>
