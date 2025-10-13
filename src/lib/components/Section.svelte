447b55d0605a60f2366260bf586744f030253bab
chrisknsmn <116185710+chrisknsmn@users.noreply.github.com>
2025-10-13 13:07:20 -0400

    button and section components

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let variant: 'default' | 'fit' = 'default';
+</script>
+
+<section
+	class="
+    w-full border-4
+    {variant === 'default' ? 'min-h-screen' : ''}
+    {variant === 'fit' ? 'min-h-fit' : ''}
+    "
+>
+	<slot />
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-    import Button from '$lib/components/Button.svelte';
-    let num = 0;
-    function add() {
-        num += 1;
-    }
+	import Button from '$lib/components/Button.svelte';
+	let num = 0;
+	function add() {
+		num += 1;
+	}
 </script>
 
 <div class="min-h-screen min-w-screen flex items-center justify-center">
-    <div class="flex flex-col gap-4">
-	<h1 class="text-4xl font-bold">HELLO WORLD!</h1>
-    <Button on:click={add}>{num}</Button>
-    </div>
+	<div class="flex flex-col gap-4">
+		<h1 class="text-4xl font-bold">HELLO WORLD!</h1>
+		<Button on:click={add}>{num}</Button>
+	</div>
 </div>
